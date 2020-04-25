9b6c5ccdff32051510e6930de3092f43546ba01d
SH13LDS <coalshields@gmail.com>
2020-04-25 16:21:33 -0500

    Basic routes and responses setup

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
-// LOAD ENV VARIABLES
 dotenv.config({ path: './config/config.env' });
 
-// INITIALIZE APP VARIABLE WITH EXPRESS
 const app = express();
+
+app.get('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+app.get('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create a new bootcamp' });
+});
+
+app.put('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete('/api/v1/bootcamps/:id', (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Delete bootcamp ${req.params.id}` });
+});
 
 const PORT = process.env.PORT || 5000;
 
