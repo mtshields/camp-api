841ff1853203335ab954a92848a213bc5b8f193d
SH13LDS <coalshields@gmail.com>
2020-04-28 12:12:42 -0500

    GET single bootcamp - added handling for querying bootcamp with an invalid id

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -22,7 +22,14 @@ exports.getBootcamps = async (req, res, next) => {
 // @access  Public
 exports.getBootcamp = async (req, res, next) => {
   try {
-    const bootcamp = await Bootcamp.find(req.paramsid);
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    if (!bootcamp) {
+      return res.status(400).json({
+        success: false,
+      });
+    }
+
     res.status(200).json({
       success: true,
       data: bootcamp,
