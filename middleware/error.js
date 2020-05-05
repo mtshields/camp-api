f3c02c24ee1f11ff7473ed26bf40d6a6d73bcced
SH13LDS <coalshields@gmail.com>
2020-05-04 23:57:36 -0500

    Refactor error.js errorHandler with ErrorResponse and implement the refactor in bootcamps.js

--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,21 @@
+const ErrorRespone = require('../utils/ErrorResponse');
+
 const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+  error.message = err.message;
+
   // Log to console for the developer
   console.log(err);
 
-  res.status(err.statusCode || 500).json({
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorRespone(message, 404);
+  }
+
+  res.status(error.statusCode || 500).json({
     success: false,
-    error: err.message || 'Server error',
+    error: error.message || 'Server error',
   });
 };
 
