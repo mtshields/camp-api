1be4830c49e30e0587a779f10be205ef1f8e3322
SH13LDS <coalshields@gmail.com>
2020-05-05 22:13:34 -0500

    Error handling for Mongoose bad ObjectId

--- middleware/error.js
+++ middleware/error.js
@@ -1,4 +1,4 @@
-const ErrorRespone = require('../utils/ErrorResponse');
+const ErrorResponse = require('../utils/ErrorResponse');
 
 const errorHandler = (err, req, res, next) => {
   let error = { ...err };
