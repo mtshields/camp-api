67023de85af4d2e747afca0eb0fbc201b1d23a20
SH13LDS <coalshields@gmail.com>
2020-05-27 10:29:15 -0500

    Course routes and controller. GET all courses & GET courses by bootcamp

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const {
   getBootcamps,
   getBootcamp,
@@ -8,6 +7,14 @@ const {
   deleteBootcamp,
   getBootcampsInRadius,
 } = require('../controllers/bootcamps');
+
+// Include other resource routers
+const courseRouter = require('./courses');
+
+const router = express.Router();
+
+// Re-route into other resource routers
+router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
