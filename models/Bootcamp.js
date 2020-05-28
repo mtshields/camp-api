84da395849b1330e7490980d93c9f006e5df7113
SH13LDS <coalshields@gmail.com>
2020-05-27 23:19:51 -0500

    Implement cascade delete of courses when a bootcamp is deleted

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -135,6 +135,13 @@ BootcampSchema.pre('save', async function (next) {
   next();
 });
 
+// Cascade delete courses when a bootcamp is deleted
+BootcampSchema.pre('remove', async function (next) {
+  console.log(`Courses being removed from bootcamp ${this._id}`);
+  await this.model('Course').deleteMany({ bootcamp: this._id });
+  next();
+});
+
 // Reverse populate with mongoose virtuals
 BootcampSchema.virtual('courses', {
   ref: 'Course',
