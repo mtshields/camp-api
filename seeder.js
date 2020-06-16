3e8883a26a43191db8da56e298256f83ee599503
SH13LDS <coalshields@gmail.com>
2020-06-16 16:25:35 -0500

    Load User model, read JSON file, import data into database

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 // Load models
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to database
 mongoose.connect(process.env.MONGOOSE_URI, {
@@ -26,11 +27,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import data into database
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
     console.log('Data imported to database...');
     process.exit();
   } catch (error) {
@@ -43,6 +49,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
     console.log('Data deleted from database...');
     process.exit();
   } catch (error) {
